0bd5bc8dd94c8f691aa56f468adffaaef7bfced9
MattAHogben <mattahogben@gmail.com>
2018-04-09 13:22:30 +1000

    First push
    -- Still to do.
    
    HTTPS
    Multiple Segment Download

---
+++ hlsDownload.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Name of the containing folder
+GROUP="My Videos"
+
+# INSERT LINKS TO VIDEOS HERE, in format as below
+LINK=(
+'Title for the video'
+'http://s2.content.video.llnw.net/smedia/42f4e71183054396907c0dea18241568/yd/yLpoUEilVrxhhYSkpsyGMnn9t0N3AYxWmoMsM7Faw/francstireurs_entrevue_ep472_seq24.mpegts/playlist'
+)
+
+# ------------------------------------------------------------------------------
+mkdir "$GROUP"
+cd "$GROUP"
+
+I=0
+while [ $I -lt ${#LINK[@]} ]
+do
+  # create folder for streaming media
+  TITLE=${LINK[$I]}
+  mkdir "$TITLE"
+  cd "$TITLE"
+  mkdir 'parts'
+  cd 'parts'
+
+  J=$((I + 1))
+  URL=${LINK[$J]}
+
+  I=$((I + 2))
+
+  DIR="${URL##*/}"
+
+  # download all streaming media parts
+  VID=-1
+  while [ $? -eq 0 ];
+  do
+    VID=$((VID + 1))
+    wget $URL''$VID.ts
+  done
+done
